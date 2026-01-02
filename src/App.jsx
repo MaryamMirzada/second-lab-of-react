bb292e9cfbd0d575bc5f922b7c3ad78a7da1c077
Maryam Mirzada <marimmirzada@gmail.com>
2026-01-02 10:31:11 +0430

    it is the second lesson of react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Greeting from './component/Greeting'
+
+function App(){
+  return (
+    <div>
+      <Greeting name="Maryam" role="Teacher"></Greeting>
+      <Greeting name="Nasiba" role="Doctor"></Greeting>
+      <Greeting name="Nasrat" role="Nurse"></Greeting>
+      <Greeting name="Jawid" role="Manager"></Greeting>
+      <Greeting name="Jawid" role="Manager"></Greeting>
+    </div>
+  );
+
+}
+export default App
---
+++ src/component/Greeting.jsx
@@ -0,0 +1,14 @@
+function Greeting ({name, role}){
+    return (
+        <div>
+    <h5>
+        Hello {name}
+    </h5>
+    <p>
+        you are {role} in the Swedish org
+    </p>
+        </div>
+    )
+}
+
+export default Greeting 
