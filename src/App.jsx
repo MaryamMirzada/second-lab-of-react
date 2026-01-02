e3edf912c83d94ccdbe2afb07f7953b9d78d95d7
Maryam Mirzada <marimmirzada@gmail.com>
2026-01-02 10:39:33 +0430

    create resuble component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import './App.css'
 import Greeting from './component/Greeting'
+import Info from './component/Info'
 
 function App(){
   return (
+    // <div>
+    //   <Greeting name="Maryam" role="Teacher"></Greeting>
+    //   <Greeting name="Nasiba" role="Doctor"></Greeting>
+    //   <Greeting name="Nasrat" role="Nurse"></Greeting>
+    //   <Greeting name="Jawid" role="Manager"></Greeting>
+    //   <Greeting name="Jawid" role="Manager"></Greeting>
+    // </div>
     <div>
-      <Greeting name="Maryam" role="Teacher"></Greeting>
-      <Greeting name="Nasiba" role="Doctor"></Greeting>
-      <Greeting name="Nasrat" role="Nurse"></Greeting>
-      <Greeting name="Jawid" role="Manager"></Greeting>
-      <Greeting name="Jawid" role="Manager"></Greeting>
+      <Info name=" Shabnam" site="Kabul" role="student" org="FBH"></Info>
+      <Info
+        name="Maryam"
+        site="Parwan"
+        role="Midwife"
+        org="Swedish committee"
+      ></Info>
+      <Info
+        name="Muzhda"
+        site="Kapisa"
+        role="enginneer"
+        org="WFP"
+      ></Info>
     </div>
   );
 
---
+++ src/component/Info.jsx
@@ -0,0 +1,10 @@
+function Info ({name, site, role, org }){
+    return (
+      <div>
+        <p>
+          this is {name} from {site}. I am {role} in the {org}. 
+        </p>
+      </div>
+    );
+}
+export default Info 
